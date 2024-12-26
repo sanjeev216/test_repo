d21f3f86ed4cbe0f0d8320575aefbb54d324a68f
sanjeev216 <36722829+sanjeev216@users.noreply.github.com>
2024-12-26 18:50:13 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,13 @@
+# Defining main function
+
+import os
+
+def main():
+    print("hey there Sanjeev")
+    print(os.getcwd())
+
+
+# Using the special variable 
+# __name__
+if __name__=="__main__":
+    main()
